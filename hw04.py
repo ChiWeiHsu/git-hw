05551f69f3bbed928cf3b6a14a1c374c482323f3
ChiaWeiHsu <s658775@gmail.com>
2017-11-29 22:39:54 +0800

    upload homework

---
+++ hw04.py
@@ -0,0 +1,12 @@
+﻿def multiplication_table(m, n):
+    S=""
+    for i in range(1,10):
+        for j in range(m,n+1):
+            S=(S+str(j)+"x"+str(i)+"="+str(i*j)+"\t")
+        S=S+"\n"
+    print (S)
+
+def pyramid(h):
+    for i in range(h):
+        print(" "*(h-i)+"*"*(i)*2+"*")
+    
